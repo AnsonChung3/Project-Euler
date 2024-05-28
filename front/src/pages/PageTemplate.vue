6476f6bd680c9b51bc5aef78f9cbe248cda85305
Anson Chung <ansonchung3@gmail.com>
2024-05-28 20:55:44 +0100

    Added example to template

--- front/src/pages/PageTemplate.vue
+++ front/src/pages/PageTemplate.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
         <h1>{{ TITLE }}</h1>
+        <div>
+            <q-input
+                v-model="INPUT"
+                dark
+                outline
+            />
+            <p>This is what you typed in the v-modle input field above:</p>
+            <p>{{ INPUT }}</p>
+        </div>
         <q-btn
             label="test docker"
             @click="test"
@@ -9,7 +18,11 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
 const TITLE = 'Hello! This is template page!';
+const INPUT = ref(undefined);
+
 </script>
 
 <style scoped>
