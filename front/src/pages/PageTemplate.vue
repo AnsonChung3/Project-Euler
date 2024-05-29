41e93eef1263d96647b66092a6f3e38f1a094d88
Anson Chung <ansonchung3@gmail.com>
2024-05-29 20:36:52 +0100

    Added test/example for GET request for backend

--- front/src/pages/PageTemplate.vue
+++ front/src/pages/PageTemplate.vue
@@ -16,6 +16,10 @@
         </div>
         <div>
             <q-btn
+                label="test Docker Get"
+                @click="testDockerGet"
+            />
+            <q-btn
                 label="test text"
                 @click="testText"
             />
@@ -25,6 +29,7 @@
 
 <script setup>
 import { ref } from 'vue';
+import { api } from 'src/boot/axios.js';
 
 const TITLE = 'Hello! This is template page!';
 const INPUT = ref(undefined);
@@ -32,6 +37,17 @@ const DISPLAY = ref('Display is empty');
 
 function testText () {
     DISPLAY.value = 'test';
+}
+
+function testDockerGet () {
+    console.log('test docker get');
+    api.get('api/test')
+    .then(response => {
+        DISPLAY.value = response.data;
+    })
+    .catch(error => {
+        DISPLAY.value = error;
+    })
 }
 
 </script>
