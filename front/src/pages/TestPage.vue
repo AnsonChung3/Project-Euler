0a8e0366b73d79bcf16b7fabb58d2ae7c71e20c3
Anson Chung <ansonchung3@gmail.com>
2024-05-30 17:08:10 +0100

    Added Test page

---
+++ front/src/pages/TestPage.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>{{ TITLE }}</h1>
+    </div>
+</template>
+
+<script setup>
+const TITLE = 'Test Page';
+</script>
--- front/src/router/routes.js
+++ front/src/router/routes.js
@@ -14,6 +14,13 @@ const routes = [
             { path: "", component: () => import("pages/PageTemplate.vue") }
         ]
     },
+    {
+        path: "/test_page",
+        component: () => import("layouts/SolarizedDarkLayout.vue"),
+        children: [
+            { path: "", component: () => import("pages/TestPage.vue") }
+        ]
+    },
 
     // Always leave this as last one,
     // but you can also remove it
