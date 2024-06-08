71d6be635bed79bfe7dcfb2ab5e24a6348d429b7
Anson Chung <ansonchung3@gmail.com>
2024-06-08 20:21:34 +0100

    Added helper function for generating indefinite number of prime numbers

--- back/helpers.py
+++ back/helpers.py
@@ -1,2 +1,17 @@
 def demo_helper():
     return 200
+
+def gen_primes():
+    D = {}
+    q = 2
+    
+    while True:
+        if q not in D:
+            yield q
+            D[q * q] = [q]
+        else:
+            for p in D[q]:
+                D.setdefault(p + q, []).append(p)
+            del D[q]
+        
+        q += 1  
